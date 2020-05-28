afa1eac12fef04cce0161abc5e889379155006f8
marvinlopez <54511822+mlopez0@users.noreply.github.com>
2020-05-28 13:18:29 -0700

    FR 1.10 Repository Rules/PRules

---
+++ EnergyAndMaterialBalanceModule/Data/Repositories/IPruleRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnergyAndMaterialBalanceModule.Data.Repositories
+{
+    public interface IPruleRepository
+    {
+        Task<IEnumerable<Models.Prule>> GetAllRules();
+
+    }
+}
---
+++ EnergyAndMaterialBalanceModule/Data/Repositories/RulesRepository.cs
@@ -0,0 +1,29 @@
+﻿using EnergyAndMaterialBalanceModule.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnergyAndMaterialBalanceModule.Data.Repositories
+{
+    public class RulesRepository : BaseRepository<Rules, SEICBalanceContext>, IResourcesRepository
+    {
+        public RulesRepository(SEICBalanceContext context) : base(context)
+        {
+
+        }
+
+        public async Task<IEnumerable<Resources>> GetAllResources()
+        {
+            return (IEnumerable<Resources>)await GetAll().ToListAsync();
+        }
+
+        public async Task<IEnumerable<Resources>> GetAllFormulas()
+        {
+            return (IEnumerable<Resources>)await GetAll().ToListAsync();
+
+        }
+
+    }
+}
